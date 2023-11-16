5f1de2cd2fc7ef6092e73fd63bac7dbeb0abea66
CWEIB <44942392+CWEIB@users.noreply.github.com>
2023-11-16 11:30:07 +0800

    Fix the error with the upload animation.
    Fix the error of not adding animation frames when uploading animated images.

--- dotpack/ledbag.py
+++ dotpack/ledbag.py
@@ -678,6 +678,7 @@ class Animation:
             )
 
             if to_pack._ledpanel:
+                filename = str(self.num_frames)+"_"+filename
                 to_pack._update_animation(filename, self.frames)
                 to_pack._display_gif(1, filename)
 
@@ -735,6 +736,8 @@ class Animation:
         """load gif"""
         im = Image.open(gif_image)
         self.frames = self._resize_gif(im)
+        self.num_frames = len(self.frames)
+
 
     def _resize_gif(self, im):
         frames = [
